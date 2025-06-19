152d7e01915f406bda3bc76ef6e41e5673d9603d
Shreyash <shreyashiitr22@gmail.com>
2025-06-19 23:21:12 +0530

    added try-catch logic

--- Swiftpay/apps/user-app/app/api/user/route.ts
+++ Swiftpay/apps/user-app/app/api/user/route.ts
@@ -3,12 +3,22 @@ import { NextResponse } from "next/server"
 import { authOptions } from "../../lib/auth"
 
 export const GET=async()=>{
+   try{
+
+    
     const session=await getServerSession(authOptions)
     if(session && session.user){
         return NextResponse.json({
             user:session.user
         })
     }
+   }
+   catch(e){ return NextResponse.json({
+        message:"You arent logged in"
+    },
+{
+    status:403
+})} 
     return NextResponse.json({
         message:"You arent logged in"
     },
