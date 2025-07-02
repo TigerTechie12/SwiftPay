2ab04e2c8399e6d0a78db5467f90546876d25154
Shreyash <shreyashiitr22@gmail.com>
2025-07-02 14:19:05 +0530

    Finished onRamp

--- Swiftpay/apps/user-app/components/AddMoneyCard.tsx
+++ Swiftpay/apps/user-app/components/AddMoneyCard.tsx
@@ -5,6 +5,7 @@ import { Center } from "@repo/ui/center";
 import { Select } from "@repo/ui/select";
 import { useState } from "react";
 import { TextInput } from "@repo/ui/textinput";
+import { createOnRampTransacation } from "../app/lib/actions/createOnRampTrans";
 
 const SUPPORTED_BANKS = [{
     name: "HDFC Bank",
@@ -14,24 +15,28 @@ const SUPPORTED_BANKS = [{
     redirectUrl: "https://www.axisbank.com/"
 }];
 
-export const AddMoney = () => {
+export async function AddMoney() {
     const [redirectUrl, setRedirectUrl] = useState(SUPPORTED_BANKS[0]?.redirectUrl);
+    const [amount,setAmount]=useState(0)
+    const [provider,setProvider]=useState(SUPPORTED_BANKS [0]?.name || '')
     return <Card title="Add Money">
     <div className="w-full">
-        <TextInput label={"Amount"} placeholder={"Amount"} onChange={() => {
-
+        <TextInput label={"Amount"} placeholder={"Amount"} onChange={(value:any) => {
+          setAmount(value)
         }} />
         <div className="py-4 text-left">
             Bank
         </div>
         <Select onSelect={(value) => {
             setRedirectUrl(SUPPORTED_BANKS.find(x => x.name === value)?.redirectUrl || "")
+            setProvider(SUPPORTED_BANKS.find(x => x.name === value)?.redirectUrl || "")
         }} options={SUPPORTED_BANKS.map(x => ({
             key: x.name,
             value: x.name
         }))} />
         <div className="flex justify-center pt-4">
-            <Button onClick={() => {
+            <Button onClick={async() => {
+             await createOnRampTransacation(amount * 100,provider)
                 window.location.href = redirectUrl || "";
             }}>
             Add Money
